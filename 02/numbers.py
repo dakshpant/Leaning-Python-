766b0ccd6daa303c7b41375ef37ed5bc54ea39c1
Daksh Pant <125189255+dakshpant@users.noreply.github.com>
2026-06-07 20:18:19 +0530

    Numbers in python

---
+++ 02/numbers.py
@@ -0,0 +1,36 @@
+x=2
+y=3
+z=4
+x+y
+# 5
+x+y*z
+# 14
+x+y*z
+# 14
+(x+y)*z
+# 20
+
+40 + 2.23
+# 42.23
+'daksh'+23
+# Traceback (most recent call last):
+#   File "<python-input-10>", line 1, in <module>
+#     'daksh'+23
+#     ~~~~~~~^~~
+# TypeError: can only concatenate str (not "int") to str
+float(40)
+# 40.0
+'chai'+'code'
+# 'chaicode' => OPERATROR OVERLOADING
+
+x, y, z
+# (2, 3, 4) => prints in tuple format
+
+
+repr('chai')
+# "'chai'" => gives the string representation of the object for debugging purposes or serialization gives raw string representation of the object
+str('chai')
+# 'chai' => gives the informal string representation of the object for human-readable purposes
+
+print('chai')
+# chai => prints the string without quotes uses strig default 
